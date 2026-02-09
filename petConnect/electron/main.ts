70213f41e7855d9587239a54d3a44fb010457b61
andreasp31 <andreasofiapais@gmail.com>
2026-02-09 01:28:03 +0100

    PrimeraPantallaVue

---
+++ petConnect/electron/main.ts
@@ -0,0 +1,69 @@
+import { app, BrowserWindow } from 'electron'
+import { createRequire } from 'node:module'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const require = createRequire(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.mjs
+// │
+process.env.APP_ROOT = path.join(__dirname, '..')
+
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+
+let win: BrowserWindow | null
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    useContentSize: true, 
+    resizable: false,   
+    autoHideMenuBar: true,
+    
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.mjs")
+    }
+  });
+  win.webContents.openDevTools()
+  win.setMenuBarVisibility(false);
+
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (new Date()).toLocaleString());
+  });
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+    win = null
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ petConnect/src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="contenedor">
+    <div class="redesSociales">
+      <div id="icono1" class="icono"></div>
+      <div id="icono2" class="icono"></div>
+      <div id="icono3" class="icono"></div>
+    </div>
+    <div class="bloqueIzquierdo">
+      <img src="./assets/logoPet.png" class="logo">
+      <div>
+        <h2 class="texto">Más que un refugio, un punto de encuentro.</h2>
+      <h2 class="texto">Conecta con nuestros animales a través de paseos, juegos y actividades guiadas diseñadas para fortalecer su felicidad y la tuya.</h2>
+      </div>
+      <div class="botones">
+        <button class="botonPrimario">Iniciar sesión</button>
+        <button class="botonPrimario">Registrarse</button>
+      </div>
+    </div>
+    <div class="bloqueDerecho">
+      <img src="./assets/fotoPerro2.png" class="foto">
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const nuevaTarea = ref('')
+const tareas = ref(['Analizar requisitos', 'Diseñar DB en Mongo'])
+
+const addTarea = () => {
+  if(nuevaTarea.value) {
+    tareas.value.push(nuevaTarea.value)
+    nuevaTarea.value = ''
+  }
+}
+</script>
+
+<style>
+  .contenedor{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .botones{
+    display: flex;
+    flex-direction: column;
+    width: 20em;
+    gap:2em;
+  }
+  .botonPrimario{
+    height: 4em;
+    background-color: #eec699;
+    color: #110501;
+    border-radius: 15em;
+  }
+  .texto{
+    color: #110501;
+    font-weight: 100;
+    width: 28em;
+  }
+  .redesSociales{
+    display:flex;
+    gap: 1.3em;
+    width: 98%;
+    justify-content: end;
+  }
+  .icono{
+    height: 2.2em;
+    width: 2.2em;
+    background-color: #110501;
+    -webkit-mask-repeat: no-repeat;
+    mask-repeat: no-repeat;
+    -webkit-mask-size: contain;
+    mask-size: contain;
+  }
+  #icono1{
+    -webkit-mask-image: url(./assets/facebook.svg);
+    mask-image: url(./assets/facebook.svg);
+  }
+  #icono2{
+    -webkit-mask-image: url(./assets/Instagram.svg);
+    mask-image: url(./assets/Instagram.svg);
+  }
+  #icono3{
+    -webkit-mask-image: url(./assets/tiktok.svg);
+    mask-image: url(./assets/tiktok.svg);
+  }
+  .icono:hover{
+    background-color: #eec699;
+    cursor: pointer;
+    transform: scale(1.2);
+  }
+  .bloqueIzquierdo{
+    display: flex;
+    flex-direction: column;
+    gap:2em;
+    align-items: center;
+    width: 60%;
+    margin-top: 5em;
+  }
+  .bloqueDerecho{
+    margin-top: -29em;
+    width: 60em;
+    align-content: end;
+    align-self: flex-end;
+  }
+  .foto{
+    width: 50em;
+  }
+  .logo{
+    object-fit:cover;
+    height: 13em;
+  }
+</style>
